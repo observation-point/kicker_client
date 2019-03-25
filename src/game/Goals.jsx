d3af11f43ac23968b3848a0b4f8d153bd5f5fff7
Adil <adilserkerov@gmail.com>
2019-03-26 00:28:00 +0300

    asdfasdf

--- src/game/Goals.jsx
+++ src/game/Goals.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Goals = props => {
     return (
         <>
-            <div style="color: red">{props.goals.filter(item => item.side === 'RED').length}</div>
+            <div style={{color: 'red'}}>{props.goals.filter(item => item.side === 'RED').length}</div>
             -
-            <div style="color: black">{props.goals.filter(item => item.side === 'BLACK').length}</div>
+            <div style={{color: 'black'}}>{props.goals.filter(item => item.side === 'BLACK').length}</div>
         </>
     );
 };
