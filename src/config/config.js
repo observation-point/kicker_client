7959e36932440abba948fd5edfcfdcae501e9066
Adil <adilserkerov@gmail.com>
2019-04-01 21:18:34 +0300

    dfdf

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
+const apiUrl = window.location.hostname === 'kicker.lan' ?
+    `http://${window.location.hostname}/api` :
+    `http://${window.location.hostname}:8888/api`;
+
 export default {
-    api_url: `http://${window.location.hostname}:8888/api`,
+    api_url: apiUrl,
     socket_url: `http://${window.location.hostname}:8889`
 }
