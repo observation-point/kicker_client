0bb62bbcb386d229529430b11349318695a1c1a3
Pom4H <1@rom4.ru>
2019-05-16 21:37:53 +0300

    fix: replay url

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -4,13 +4,14 @@ import ReactPlayer from 'react-player';
 class VideoPlayer extends Component {
     render() {
         const { gameId, goalId } = this.props;
+
         const style = {
             position: 'absolute',
             zIndex: -1
         };
         const replayUrl =
             gameId && gameId
-                ? `http://www.130.lan:8888/${gameId}/${goalId}.mp4`
+                ? `http://www130.lan:8888/${gameId}/${goalId}.mp4`
                 : 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
         return <ReactPlayer style={style} url={replayUrl} playing />;
     }
